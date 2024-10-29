10ab8cc2432ead0f2ba5a2e64926ccb045478e40
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-10-29 21:46:43 +0300

    Update Seeding

--- EagleTech_Task.Presistance/Seeding/DataSeeding.cs
+++ EagleTech_Task.Presistance/Seeding/DataSeeding.cs
@@ -1,4 +1,5 @@
-﻿using EagleTech_Task.Domain.Constant;
+﻿using EagleTech_Task.Application.Interfaces.Auth;
+using EagleTech_Task.Domain.Constant;
 using EagleTech_Task.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,7 @@ namespace EagleTech_Task.Presistance.Seeding
         public async static void Initialize(IServiceProvider serviceProvider)
         {
             var dbContext = serviceProvider.GetRequiredService<OrderMangmentDbContext>();
+            var authService = serviceProvider.GetRequiredService<IAuthServices>();
 
             if (dbContext.Database.GetAppliedMigrations().Count() == 0)
             {
@@ -18,6 +20,23 @@ namespace EagleTech_Task.Presistance.Seeding
                 await dbContext.AddAsync(new Role() { Name = Constants.Admin });
                 await dbContext.AddAsync(new Role() { Name = Constants.Supplier });
                 await dbContext.AddAsync(new Role() { Name = Constants.TechnicalSupport });
+
+                await dbContext.SaveChangesAsync();
+
+                authService.CreatePasswordHash("123@Abc", out var passwordHash, out var passwordSalt);
+
+                var adminRole = await dbContext.Roles.SingleAsync(x => x.Name == Constants.Admin);
+
+                await dbContext.Users.AddAsync(new()
+                {
+                    UserName="OmarAlaa",
+                    Email = "omar@test.com",
+                    FirstName = "omar",
+                    LastName = "alaa",
+                    PasswordHash = passwordHash,
+                    Roles = [adminRole],
+                    PasswordSalt = passwordSalt
+                });
 
                 await dbContext.SaveChangesAsync();
             }
